13e2755f54b10dd6d16dd5f2d9a454201a9c5be1
HoangTran1807 <tranhuuhoang692@gmail.com>
2024-06-25 15:37:56 +0700

    client ip

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const os = require('os');
 const app = express();
-const PORT = 3000;
-
-const networkInterfaces = os.networkInterfaces();
-
-
-
 
 app.get('/', (req, res) => {
-    for (const name in networkInterfaces) {
-        const networkInterface = networkInterfaces[name];
-        for (const address of networkInterface) {
-          if (address.family === 'IPv4') {
-            console.log(`Server IP address: ${address.address}`);
-          }
-        }
-      }
-    res.send('Hello World!');
+    const clientIp = req.ip;
+    console.log('Client IP:', clientIp);
+    res.send(`Your IP address is ${clientIp}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
